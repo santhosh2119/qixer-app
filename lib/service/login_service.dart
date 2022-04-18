764756d746b526440f2566758f4b6107fc32d0d6
saleheen1 <smsaleheen3@gmail.com>
2022-04-18 11:51:10 +0600

    login api implemented

---
+++ lib/service/login_service.dart
@@ -0,0 +1,93 @@
+import 'dart:convert';
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:qixer/view/home/landing_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../view/utils/constant_colors.dart';
+import '../view/utils/others_helper.dart';
+
+class LoginService with ChangeNotifier {
+  bool isloading = false;
+
+  setLoadingTrue() {
+    isloading = true;
+    notifyListeners();
+  }
+
+  setLoadingFalse() {
+    isloading = false;
+    notifyListeners();
+  }
+
+  Future<bool> login(email, pass, BuildContext context, bool keepLoggedIn,
+      {isFromLoginPage = true}) async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.none) {
+      OthersHelper()
+          .showToast("Please turn on your internet connection", Colors.black);
+      return false;
+    } else {
+      setLoadingTrue();
+      var data = jsonEncode({
+        'email': email,
+        'password': pass,
+      });
+      var header = {
+        //if header type is application/json then the data should be in jsonEncode method
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      };
+
+      var response = await http.post(Uri.parse('$baseApi/login'),
+          body: data, headers: header);
+
+      if (response.statusCode == 201) {
+        if (isFromLoginPage) {
+          OthersHelper()
+              .showToast("Login successful", ConstantColors().successColor);
+        }
+        setLoadingFalse();
+
+        Navigator.pushReplacement<void, void>(
+          context,
+          MaterialPageRoute<void>(
+            builder: (BuildContext context) => const LandingPage(),
+          ),
+        );
+
+        String token = jsonDecode(response.body)['token'];
+
+        if (keepLoggedIn) {
+          saveDetails(email, pass, token);
+        } else {
+          setKeepLoggedInFalse();
+        }
+
+        return true;
+      } else {
+        //Login unsuccessful ==========>
+        if (isFromLoginPage) {
+          OthersHelper().showToast(
+              "Invalid Email or Password", ConstantColors().warningColor);
+        }
+        setLoadingFalse();
+        return false;
+      }
+    }
+  }
+
+  saveDetails(String email, pass, String token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("email", email);
+    prefs.setBool('keepLoggedIn', true);
+    prefs.setString("pass", pass);
+    prefs.setString("token", token);
+  }
+
+  setKeepLoggedInFalse() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('keepLoggedIn', false);
+  }
+}
