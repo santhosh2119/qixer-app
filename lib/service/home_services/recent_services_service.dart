896abe5e68ba08f5e5e26c2e55af364115bb70ed
saleheen1 <smsaleheen3@gmail.com>
2022-04-24 12:39:08 +0600

    home recent service saved button updating real time

--- lib/service/home_services/recent_services_service.dart
+++ lib/service/home_services/recent_services_service.dart
@@ -4,58 +4,99 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:qixer/model/recent_service_model.dart';
 import 'package:qixer/service/common_service.dart';
+import 'package:qixer/service/db/db_service.dart';
 import 'package:qixer/view/utils/others_helper.dart';
 import 'package:http/http.dart' as http;
 
 class RecentServicesService with ChangeNotifier {
-  var recentService;
-
-  List imageList = [];
-  List ratingList = [];
-  // List reviewerImgList = [];
+  var recentServiceMap = [];
+  bool alreadySaved = false;
 
   fetchRecentService() async {
-    if (recentService == null) {
+    if (recentServiceMap.isEmpty) {
       var connection = await checkConnection();
       if (connection) {
         //if connection is ok
         var response = await http.get(Uri.parse('$baseApi/latest-services'));
 
         if (response.statusCode == 201) {
-          recentService =
-              RecentServiceModel.fromJson(jsonDecode(response.body));
+          var data = RecentServiceModel.fromJson(jsonDecode(response.body));
 
-          for (int i = 0; i < recentService.serviceImage.length; i++) {
-            imageList.add(recentService.serviceImage[i].imgUrl);
-            // reviewerImgList.add(recentService.reviewerImage[i].imgUrl);
+          for (int i = 0; i < data.latestServices.length; i++) {
+            var serviceImage;
+            if (data.serviceImage.length > i) {
+              serviceImage = data.serviceImage[i].imgUrl;
+            } else {
+              serviceImage = null;
+            }
+
             int totalRating = 0;
             for (int j = 0;
-                j < recentService.latestServices[i].reviewsForMobile.length;
+                j < data.latestServices[i].reviewsForMobile.length;
                 j++) {
               totalRating = totalRating +
-                      recentService.latestServices[i].reviewsForMobile[j].rating
-                  as int;
+                  data.latestServices[i].reviewsForMobile[j].rating!.toInt();
             }
 
             double averageRate = 0;
 
-            if (recentService.latestServices[i].reviewsForMobile.isNotEmpty) {
+            if (data.latestServices[i].reviewsForMobile.isNotEmpty) {
               averageRate = (totalRating /
-                  recentService.latestServices[i].reviewsForMobile.length);
+                  data.latestServices[i].reviewsForMobile.length);
             }
 
-            ratingList.add(averageRate);
+            setServiceList(
+                data.latestServices[i].id,
+                data.latestServices[i].title,
+                data.latestServices[i].sellerForMobile.name,
+                data.latestServices[i].price,
+                averageRate,
+                serviceImage,
+                i);
 
             // print(averageRate);
           }
           notifyListeners();
         } else {
           //Something went wrong
-          recentService == 'error';
+          recentServiceMap.add('error');
+          notifyListeners();
         }
       }
     } else {
       //already loaded from api
     }
+  }
+
+  setServiceList(serviceId, title, sellerName, price, rating, image, index) {
+    recentServiceMap.add({
+      'serviceId': serviceId,
+      'title': title,
+      'sellerName': sellerName,
+      'price': price,
+      'rating': rating,
+      'image': image,
+      'isSaved': false,
+    });
+
+    checkIfAlreadySaved(serviceId, title, sellerName, index);
+  }
+
+  checkIfAlreadySaved(serviceId, title, sellerName, index) async {
+    var newListMap = recentServiceMap;
+    alreadySaved = await DbService().checkIfSaved(serviceId, title, sellerName);
+    newListMap[index]['isSaved'] = alreadySaved;
+    recentServiceMap = newListMap;
+    notifyListeners();
+  }
+
+  saveOrUnsave(int serviceId, String title, String image, int price,
+      String sellerName, double rating, int index, BuildContext context) async {
+    var newListMap = recentServiceMap;
+    alreadySaved = await DbService().saveOrUnsave(
+        serviceId, title, image, price, sellerName, rating, context);
+    newListMap[index]['isSaved'] = alreadySaved;
+    recentServiceMap = newListMap;
+    notifyListeners();
   }
 }
--- lib/view/home/components/recent_services.dart
+++ lib/view/home/components/recent_services.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:qixer/service/common_service.dart';
 import 'package:qixer/service/home_services/recent_services_service.dart';
 import 'package:qixer/service/home_services/top_rated_services_service.dart';
 import 'package:qixer/view/home/components/service_card.dart';
 import 'package:qixer/view/services/service_details_page.dart';
 import 'package:qixer/view/utils/constant_colors.dart';
+import 'package:qixer/view/utils/others_helper.dart';
 
 class RecentServices extends StatelessWidget {
   const RecentServices({
@@ -16,8 +18,9 @@ class RecentServices extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<RecentServicesService>(
-      builder: (context, provider, child) => provider.recentService != null
-          ? provider.recentService != 'error'
+      builder: (context, provider, child) => provider
+              .recentServiceMap.isNotEmpty
+          ? provider.recentServiceMap[0] != 'error'
               ? Container(
                   margin: const EdgeInsets.only(top: 5),
                   height: 190,
@@ -26,9 +29,7 @@ class RecentServices extends StatelessWidget {
                     shrinkWrap: true,
                     clipBehavior: Clip.none,
                     children: [
-                      for (int i = 0;
-                          i < provider.recentService.latestServices.length;
-                          i++)
+                      for (int i = 0; i < provider.recentServiceMap.length; i++)
                         InkWell(
                           splashColor: Colors.transparent,
                           highlightColor: Colors.transparent,
@@ -43,19 +44,33 @@ class RecentServices extends StatelessWidget {
                           },
                           child: ServiceCard(
                             cc: cc,
-                            imageLink: provider.imageList[i],
-                            rating: provider.ratingList[i].toString(),
-                            title:
-                                provider.recentService.latestServices[i].title,
-                            sellerName: provider.recentService.latestServices[i]
-                                .sellerForMobile.name,
-                            price:
-                                provider.recentService.latestServices[i].price,
+                            imageLink: provider.recentServiceMap[i]['image'] ??
+                                placeHolderUrl,
+                            rating: twoDouble(
+                                provider.recentServiceMap[i]['rating']),
+                            title: provider.recentServiceMap[i]['title'],
+                            sellerName: provider.recentServiceMap[i]
+                                ['sellerName'],
+                            price: provider.recentServiceMap[i]['price'],
                             buttonText: 'Book Now',
                             width: MediaQuery.of(context).size.width - 85,
                             marginRight: 17.0,
-                            pressed: () {},
-                            isSaved: false,
+                            pressed: () {
+                              provider.saveOrUnsave(
+                                  provider.recentServiceMap[i]['serviceId'],
+                                  provider.recentServiceMap[i]['title'],
+                                  provider.recentServiceMap[i]['image'],
+                                  provider.recentServiceMap[i]['price'],
+                                  provider.recentServiceMap[i]['sellerName'],
+                                  twoDouble(
+                                      provider.recentServiceMap[i]['rating']),
+                                  i,
+                                  context);
+                            },
+                            isSaved:
+                                provider.recentServiceMap[i]['isSaved'] == true
+                                    ? true
+                                    : false,
                           ),
                         )
                     ],
