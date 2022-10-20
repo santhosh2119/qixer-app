82e705d309db01372358c01b96dde894ecad1a88
saleheen1 <smsaleheen3@gmail.com>
2022-10-20 18:26:36 +0600

    live chat search profile image issue fixed

--- lib/service/live_chat/chat_list_service.dart
+++ lib/service/live_chat/chat_list_service.dart
@@ -80,9 +80,15 @@ class ChatListService with ChangeNotifier {
     for (int i = 0; i < storeChatList.length; i++) {
       if ((storeChatList[i].sellerList.name.toLowerCase())
           .contains(searchString.toLowerCase())) {
-        print(storeChatListImage[i]['img_url']);
         chatList.add(storeChatList[i]);
-        chatListImage.add(storeChatListImage[i]);
+
+        if (storeChatListImage[i] is List) {
+          //if api returned empty array then in image we insert empty array
+          //because, in frontend we validate image based on the empty array
+          chatListImage.add([]);
+        } else {
+          chatListImage.add(storeChatListImage[i]);
+        }
       }
     }
     notifyListeners();
